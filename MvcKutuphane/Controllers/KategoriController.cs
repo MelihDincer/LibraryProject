c80f7fef57be1c418cb89297ad93532c503077f2
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-22 22:40:18 +0300

    Kategori sayfası oluşturuldu.

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class KategoriController : Controller
     {
-        // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle() 
+        { 
+            return View(); 
+        }   
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p) 
+        { 
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+        public ActionResult KategoriSil(int id)
+        {
+            var deger = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(deger); 
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
