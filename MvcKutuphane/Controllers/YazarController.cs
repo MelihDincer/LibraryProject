65a12f2c9475fa31b775ebab1cde32dce4b4d69c
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-22 23:58:53 +0300

    YazarController oluşturuldu. Listeleme ve yazar ekleme işlemleri yapıldı.

---
+++ MvcKutuphane/Controllers/YazarController.cs
@@ -0,0 +1,31 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class YazarController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLYAZAR.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YazarEkle() 
+        { 
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult YazarEkle(TBLYAZAR p) 
+        { 
+            db.TBLYAZAR.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
