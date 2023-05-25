7d038d0896faef5357ca4a44650c6c19300d3f26
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-25 15:33:53 +0300

    Kitap arama, silme ve güncelleme işlemleri yapıldı.

--- MvcKutuphane/Controllers/KitapController.cs
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -10,10 +10,15 @@ namespace MvcKutuphane.Controllers
     public class KitapController : Controller
     {
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
-        public ActionResult Index()
+        public ActionResult Index(string p)
         {
-            var kitaplar = db.TBLKITAP.ToList();
-            return View(kitaplar);
+            var kitaplar = from k in db.TBLKITAP select k;
+            if (!string.IsNullOrEmpty(p) && (!string.IsNullOrWhiteSpace(p))) // gelen p değerinin boş olması ve space değerlerinden oluşmaması durumlarında
+            {
+                kitaplar = kitaplar.Where(m => m.AD.Contains(p));
+            }
+            //var kitaplar = db.TBLKITAP.ToList();
+            return View(kitaplar.ToList());
         }
         [HttpGet]
         public ActionResult KitapEkle()
@@ -44,6 +49,47 @@ namespace MvcKutuphane.Controllers
             p.TBLKATEGORI = ktg;
             p.TBLYAZAR = yzr;
             db.TBLKITAP.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KitapSil(int id)
+        {
+            var kitap = db.TBLKITAP.Find(id);
+            db.TBLKITAP.Remove(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult KitapGetir(int id)
+        {
+            var ktp = db.TBLKITAP.Find(id);
+            List<SelectListItem> deger1 = (from i in db.TBLKATEGORI.ToList()// Tblkategoriden i değeri gelecek ve 
+                                           select new SelectListItem // yeni bir liste öğesi seçilecek.
+                                           {
+                                               Text = i.AD,
+                                               Value = i.ID.ToString(),
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            List<SelectListItem> deger2 = (from i in db.TBLYAZAR.ToList()// Tblkategoriden i değeri gelecek ve 
+                                           select new SelectListItem // yeni bir liste öğesi seçilecek.
+                                           {
+                                               Text = i.AD + ' ' + i.SOYAD,
+                                               Value = i.ID.ToString(),
+                                           }).ToList();
+            ViewBag.dgr2 = deger2;
+            return View("KitapGetir", ktp);
+        }
+        public ActionResult KitapGuncelle(TBLKITAP p)
+        {
+            var kitap = db.TBLKITAP.Find(p.ID);
+            kitap.AD = p.AD;
+            kitap.BASIMYIL = p.BASIMYIL;
+            kitap.SAYFA = p.SAYFA;
+            kitap.YAYINEVI = p.YAYINEVI;
+            var ktg = db.TBLKATEGORI.Where(k => k.ID == p.TBLKATEGORI.ID).FirstOrDefault();
+            var yzr = db.TBLYAZAR.Where(y => y.ID == p.TBLYAZAR.ID).FirstOrDefault();
+            kitap.KATEGORI = ktg.ID;
+            kitap.YAZAR = yzr.ID;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
