fc00e5017cfa8a4be791812d22ba3fc4406d8821
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-26 23:48:40 +0300

    Personel Controllerı oluşturuldu. Listeleme, Silme, Güncelleme işlemleri yapıldı.

---
+++ MvcKutuphane/Controllers/PersonelController.cs
@@ -0,0 +1,58 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class PersonelController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var personel = db.TBLPERSONEL.ToList();
+            return View(personel);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(TBLPERSONEL p)
+        {
+            if (!(ModelState.IsValid))
+            {
+                return View("PersonelEkle");
+            }
+            db.TBLPERSONEL.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult PersonelSil(int id)
+        {
+            var person = db.TBLPERSONEL.Find(id);
+            db.TBLPERSONEL.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            var prs = db.TBLPERSONEL.Find(id);
+            return View("PersonelGetir", prs);
+        }
+        public ActionResult PersonelGuncelle(TBLPERSONEL p)
+        {
+            if (!(ModelState.IsValid))
+            {
+                return View("PersonelGetir");
+            }
+            var prs = db.TBLPERSONEL.Find(p.ID);
+            prs.PERSONEL = p.PERSONEL;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
