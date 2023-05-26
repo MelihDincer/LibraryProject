3986a7ab98cc84ea28d6b27f598e716fd6f8bdd0
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-26 23:50:29 +0300

    Üye ve Personel tablolarında DataAnnotations ile belirli kısıtlamalar getirildi.

--- MvcKutuphane/Models/Entity/TBLPERSONEL.cs
+++ MvcKutuphane/Models/Entity/TBLPERSONEL.cs
@@ -11,10 +11,12 @@ namespace MvcKutuphane.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TBLPERSONEL
     {
         public byte ID { get; set; }
+        [Required(ErrorMessage = "Personel Adý Boþ Geçilemez...!!")]
         public string PERSONEL { get; set; }
     }
 }
--- MvcKutuphane/Models/Entity/TBLUYELER.cs
+++ MvcKutuphane/Models/Entity/TBLUYELER.cs
@@ -11,7 +11,8 @@ namespace MvcKutuphane.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TBLUYELER
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -22,10 +23,13 @@ namespace MvcKutuphane.Models.Entity
         }
     
         public int ID { get; set; }
+        [Required(ErrorMessage = "Adý Boþ Býrakamazsýnýz!")]
+        [StringLength(20,ErrorMessage = "En Fazla 20 Karakter Girebilirsiniz!")]
         public string AD { get; set; }
         public string SOYAD { get; set; }
         public string MAIL { get; set; }
         public string KULLANICIADI { get; set; }
+        [StringLength(10, ErrorMessage = "En Fazla 10 Karakter Girebilirsiniz!")]
         public string SIFRE { get; set; }
         public string FOTOGRAF { get; set; }
         public string TELEFON { get; set; }
