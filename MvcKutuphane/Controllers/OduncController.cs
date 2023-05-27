fffa7c924795cf930ea7718ec3f2e0606e3f8a52
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-27 22:47:42 +0300

    Odunc Controllerı oluşturuldu.

---
+++ MvcKutuphane/Controllers/OduncController.cs
@@ -0,0 +1,32 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MvcKutuphane.Controllers
+{
+    public class OduncController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAREKET.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult OduncVer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult OduncVer(TBLHAREKET p)
+        {
+            db.TBLHAREKET.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
