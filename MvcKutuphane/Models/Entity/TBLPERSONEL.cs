e067fcc8b97225ac1f450c985bb6c47aef9a79f0
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-27 22:48:25 +0300

    Tablolar güncellendi.

--- MvcKutuphane/Models/Entity/TBLPERSONEL.cs
+++ MvcKutuphane/Models/Entity/TBLPERSONEL.cs
@@ -11,12 +11,19 @@ namespace MvcKutuphane.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class TBLPERSONEL
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public TBLPERSONEL()
+        {
+            this.TBLHAREKET = new HashSet<TBLHAREKET>();
+        }
+    
         public byte ID { get; set; }
-        [Required(ErrorMessage = "Personel Adý Boþ Geçilemez...!!")]
         public string PERSONEL { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TBLHAREKET> TBLHAREKET { get; set; }
     }
 }
