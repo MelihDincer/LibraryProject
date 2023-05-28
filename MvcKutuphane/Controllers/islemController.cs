fae48d269dd59fe0e9a6048ce05adde3c02c0f44
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-28 22:58:16 +0300

    İşlem menüsü için controller oluşturuldu.

---
+++ MvcKutuphane/Controllers/islemController.cs
@@ -0,0 +1,19 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class islemController : Controller
+    {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); 
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAREKET.Where(x=>x.ISLEMDURUM==true).ToList();
+            return View(degerler);
+        }
+    }
+}
