3c1ce971c9321ddd23ed4c8a1988c2ea06f3562a
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-31 23:36:56 +0300

    İstatistik Controller ı oluşturuldu.

---
+++ MvcKutuphane/Controllers/istatistikController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class istatistikController : Controller
+    {
+        // GET: istatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hava()
+        {
+            return View();
+        }
+        public ActionResult HavaKart()
+        {
+            return View();
+        }
+    }
+}
