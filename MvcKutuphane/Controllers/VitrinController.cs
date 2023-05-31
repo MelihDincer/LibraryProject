4d6eeb9b49ba8814d16273257c5c5afc4f6767ac
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-31 23:40:26 +0300

    Vitrin sayfasının controller kısmı, bir sayfada birden fazla tablodan veri alınacak şekilde  düzenlendi.

--- MvcKutuphane/Controllers/VitrinController.cs
+++ MvcKutuphane/Controllers/VitrinController.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Siniflarim;
 
 namespace MvcKutuphane.Controllers
 {
     public class VitrinController : Controller
     {
-        // GET: Vitrin
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+            cs.Deger1 = db.TBLKITAP.ToList();
+            cs.Deger2 = db.TBLHAKKIMIZDA.ToList();
+            //var degeler = db.TBLKITAP.ToList();
+            return View(cs);
+        }
+        [HttpPost]
+        public ActionResult Index(TBLILETISIM t) 
+        {
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
