87c4f575778ed4d35a869817328193bf58db73fb
Melih Dinçer <m_dincer41@hotmail.com>
2023-05-31 15:10:43 +0300

    Hakkımızda ve İletişim tabloları eklendi.

--- MvcKutuphane/Models/Entity/Model1.Context.cs
+++ MvcKutuphane/Models/Entity/Model1.Context.cs
@@ -34,5 +34,7 @@ namespace MvcKutuphane.Models.Entity
         public virtual DbSet<TBLPERSONEL> TBLPERSONEL { get; set; }
         public virtual DbSet<TBLUYELER> TBLUYELER { get; set; }
         public virtual DbSet<TBLYAZAR> TBLYAZAR { get; set; }
+        public virtual DbSet<TBLHAKKIMIZDA> TBLHAKKIMIZDA { get; set; }
+        public virtual DbSet<TBLILETISIM> TBLILETISIM { get; set; }
     }
 }
