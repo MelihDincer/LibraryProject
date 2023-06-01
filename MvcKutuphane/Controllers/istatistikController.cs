79dcad266f09af841e2f9d5642635302bd3fc765
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-01 18:34:46 +0300

    Resim yükleme işlemi yapıldı.

--- MvcKutuphane/Controllers/istatistikController.cs
+++ MvcKutuphane/Controllers/istatistikController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,6 +21,20 @@ namespace MvcKutuphane.Controllers
         public ActionResult HavaKart()
         {
             return View();
+        }
+        public ActionResult Galeri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult resimyukle(HttpPostedFileBase dosya)
+        {
+            if (dosya.ContentLength > 0)
+            {
+                string dosyayolu = Path.Combine(Server.MapPath("~/web2/resimler/"), Path.GetFileName(dosya.FileName));
+                dosya.SaveAs(dosyayolu);
+            }
+            return RedirectToAction("Galeri");
         }
     }
 }
