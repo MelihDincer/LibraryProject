25ba8aaa566c4624ce1a919e295123e39099a68e
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-02 16:39:19 +0300

    Login controllerı oluşturuldu.

---
+++ MvcKutuphane/Controllers/KayitOlController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KayitOlController : Controller
+    {
+        // GET: KayitOl
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+    }
+}
