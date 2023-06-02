c02bdab5cec1f59ffc237764130625824d943c85
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-02 13:52:55 +0300

    Index actionresult düzenlendi. LinqKart actionresult eklendi.

--- MvcKutuphane/Controllers/istatistikController.cs
+++ MvcKutuphane/Controllers/istatistikController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,9 +10,17 @@ namespace MvcKutuphane.Controllers
 {
     public class istatistikController : Controller
     {
-        // GET: istatistik
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
+            var deger1 = db.TBLUYELER.Count();
+            var deger2 = db.TBLKITAP.Count();
+            var deger3 = db.TBLKITAP.Where(x => x.DURUM == false).Count();
+            var deger4 = db.TBLCEZALAR.Sum(x => x.PARA);
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            ViewBag.dgr4 = deger4;
             return View();
         }
         public ActionResult Hava()
@@ -35,6 +44,28 @@ namespace MvcKutuphane.Controllers
                 dosya.SaveAs(dosyayolu);
             }
             return RedirectToAction("Galeri");
+        }
+        public ActionResult LinqKart()
+        {
+            var deger1 = db.TBLKITAP.Count();
+            var deger2 = db.TBLUYELER.Count();
+            var deger3 = db.TBLCEZALAR.Sum(x => x.PARA);
+            var deger4 = db.TBLKITAP.Where(x => x.DURUM == false).Count();
+            var deger5 = db.TBLKATEGORI.Count();
+            var deger11 = db.TBLILETISIM.Count();
+            var deger7 = db.TBLUYELER.Count();
+            var deger8 = db.TBLUYELER.Count();
+            var deger9 = db.TBLUYELER.Count();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+            ViewBag.dgr4 = deger4;
+            ViewBag.dgr5 = deger5;
+            ViewBag.dgr11 = deger11;
+            ViewBag.dgr7 = deger7;
+            ViewBag.dgr8 = deger8;
+            ViewBag.dgr9 = deger9;
+            return View();
         }
     }
 }
