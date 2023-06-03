62868c6907341588eb695e395db7948953a5c5d3
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-03 17:40:02 +0300

    Kayıt olma işlemi tamamlandı.

--- MvcKutuphane/Controllers/KayitOlController.cs
+++ MvcKutuphane/Controllers/KayitOlController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,21 @@ namespace MvcKutuphane.Controllers
 {
     public class KayitOlController : Controller
     {
-        // GET: KayitOl
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [HttpGet]
         public ActionResult Kayit()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(TBLUYELER p)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("Kayit");
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
             return View();
         }
     }
