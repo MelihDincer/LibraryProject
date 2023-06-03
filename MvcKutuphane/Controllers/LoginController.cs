a29eed92a8af3feb814d04fdd7cffccbedb001a6
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-03 19:43:20 +0300

    Login işlemi, başarılı giriş yapıldığında panelim sayfasına yönlendirilecek şekilde tamamlandı.

--- MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Web.Security;
 namespace MvcKutuphane.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [HttpGet]
         public ActionResult GirisYap()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult GirisYap(TBLUYELER p) 
+        {
+            var bilgiler = db.TBLUYELER.FirstOrDefault(x=>x.MAIL==p.MAIL && x.SIFRE==p.SIFRE);
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.MAIL, false);
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
