31bc2c746aa837f6f1325b6e568d5b1f3269cfc0
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-06 17:02:31 +0300

    MesajlarController oluşturuldu.

---
+++ MvcKutuphane/Controllers/MesajlarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class MesajlarController : Controller
+    {
+        // GET: Mesajlar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult YeniMesaj()
+        {
+            return View();
+        }
+    }
+}
