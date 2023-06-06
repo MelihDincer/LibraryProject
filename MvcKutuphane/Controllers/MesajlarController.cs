edc3d65e5d7636cdcda67ccbd8fea03cf9580f8e
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-06 19:11:17 +0300

    Kullanıcının kendisine gönderilen mesajların listelenmesi işlemleri yapıldı.

--- MvcKutuphane/Controllers/MesajlarController.cs
+++ MvcKutuphane/Controllers/MesajlarController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,12 @@ namespace MvcKutuphane.Controllers
     public class MesajlarController : Controller
     {
         // GET: Mesajlar
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
-            return View();
+            var kullanici = (string)Session["Mail"];
+            var mesajlar = db.TBLMESAJLAR.Where(x => x.ALICI == kullanici).ToList();
+            return View(mesajlar);
         }
         public ActionResult YeniMesaj()
         {
