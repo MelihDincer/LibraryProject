daebf09e059a5c8b4dae9a3f72e8d1751044b45c
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-10 18:05:21 +0300

    Duyuru controller eklendi. Duyuru listeleme, yeni duyuru ekleme, duyuru silme ve duyuru detaylarını görme işlemleri yapıldı.

---
+++ MvcKutuphane/Controllers/DuyuruController.cs
@@ -0,0 +1,45 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class DuyuruController : Controller
+    {
+        // GET: Duyuru
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLDUYURULAR.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniDuyuru()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniDuyuru(TBLDUYURULAR d)
+        {
+            db.TBLDUYURULAR.Add(d);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DuyuruSil(int id)
+        {
+            var duyuru = db.TBLDUYURULAR.Find(id);
+            db.TBLDUYURULAR.Remove(duyuru);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DuyuruDetay(int id)
+        {
+            var duyuru = db.TBLDUYURULAR.Find(id);
+            return View("DuyuruDetay", duyuru);
+        }
+       
+    }
+}
