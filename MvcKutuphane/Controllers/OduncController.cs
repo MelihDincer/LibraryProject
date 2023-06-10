01568aec1e37feab9b6a287d1f4e5836492bea9c
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-10 17:08:08 +0300

    Odunc verilecek kitaplarda üye,kitap,personel seçim işlemi dropdownlist ile yapılacak şekilde kodlandı.

--- MvcKutuphane/Controllers/OduncController.cs
+++ MvcKutuphane/Controllers/OduncController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,28 +13,57 @@ namespace MvcKutuphane.Controllers
     {
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
-        {           
-            var degerler = db.TBLHAREKET.Where(x=>x.ISLEMDURUM==false).ToList();
+        {
+            var degerler = db.TBLHAREKET.Where(x => x.ISLEMDURUM == false).ToList();
             return View(degerler);
         }
         [HttpGet]
         public ActionResult OduncVer()
         {
+            List<SelectListItem> deger1 = (from x in db.TBLUYELER
+                                           select new SelectListItem
+                                           {
+                                               Text = x.AD + " " + x.SOYAD,
+                                               Value = x.ID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> deger2 = (from y in db.TBLKITAP.Where(x=>x.DURUM == true)
+                                           select new SelectListItem
+                                           {
+                                               Text = y.AD,
+                                               Value = y.ID.ToString(),
+                                           }).ToList();
+
+            List<SelectListItem> deger3 = (from z in db.TBLPERSONEL
+                                           select new SelectListItem
+                                           {
+                                               Text = z.PERSONEL,
+                                               Value = z.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
             return View();
         }
         [HttpPost]
         public ActionResult OduncVer(TBLHAREKET p)
         {
+            var d1 = db.TBLUYELER.Where(x => x.ID == p.TBLUYELER.ID).FirstOrDefault();
+            var d2 = db.TBLKITAP.Where(y => y.ID == p.TBLKITAP.ID).FirstOrDefault();
+            var d3 = db.TBLPERSONEL.Where(z => z.ID == p.TBLPERSONEL.ID).FirstOrDefault();
+            p.TBLUYELER = d1;
+            p.TBLKITAP = d2;
+            p.TBLPERSONEL = d3;
             db.TBLHAREKET.Add(p);
             db.SaveChanges();
-            return View();
+            return RedirectToAction("Index");
         }
         public ActionResult Odunciade(TBLHAREKET p)
         {
             var odn = db.TBLHAREKET.Find(p.ID);
             DateTime d1 = DateTime.Parse(odn.IADETARIH.ToString());
             DateTime d2 = Convert.ToDateTime(DateTime.Now.ToShortDateString());
-            TimeSpan d3 = d2- d1;
+            TimeSpan d3 = d2 - d1;
             ViewBag.dgr = d3.TotalDays;
             return View("Odunciade", odn);
         }
