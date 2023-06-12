cb3790adf4a7bc22c47bb0f94fbd2f0bfbf0d03b
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-12 20:21:30 +0300

    Index action u viewbag ler kullanılarak güncellendi. Yeni görünüm için gerekli kodlar yazıldı. Partial1 ve Partial2  partialviewları oluşturuldu.

--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -16,9 +16,33 @@ namespace MvcKutuphane.Controllers
         public ActionResult Index()
         {
             var uyemail = (string)Session["Mail"];
-            var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            //var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            var degerler = db.TBLDUYURULAR.ToList();
+            var d1 = db.TBLUYELER.Where(x=>x.MAIL == uyemail).Select(y=>y.AD+" "+y.SOYAD).FirstOrDefault();
+            ViewBag.d1 = d1;
+            var d2 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.OKUL).FirstOrDefault();
+            ViewBag.d2 = d2;
+            var d3 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.FOTOGRAF).FirstOrDefault();
+            ViewBag.d3 = d3;
+            var d4 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.KULLANICIADI).FirstOrDefault();
+            ViewBag.d4 = d4;
+            var d5 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.TELEFON).FirstOrDefault();
+            ViewBag.d5 = d5;
+            var d6 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.MAIL).FirstOrDefault();
+            ViewBag.d6 = d6;
+            //Üyenin bu zamana kadar almış olduğu toplam kitap sayısı
+            var uyeid = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.ID).FirstOrDefault();
+            var d7 = db.TBLHAREKET.Where(x => x.UYE == uyeid).Count();
+            ViewBag.d7 = d7;
+            //Gelen mesaj sayısı
+            var d8 = db.TBLMESAJLAR.Where(x=>x.ALICI == uyemail).Count();
+            ViewBag.d8 = d8;
+            //Duyuru sayısı
+            var d9 = db.TBLDUYURULAR.Count();
+            ViewBag.d9 = d9;
             return View(degerler);
         }
+        //Üye bilgilerini güncelleme
         [HttpPost]
         public ActionResult Index2(TBLUYELER p) 
         {
@@ -50,6 +74,18 @@ namespace MvcKutuphane.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("GirisYap", "Login");
+        }
+        public PartialViewResult Partial1()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Partial2()
+        {
+            var kullanici = (string)Session["Mail"];
+            var id = db.TBLUYELER.Where(x=>x.MAIL == kullanici).Select(y=> y.ID).FirstOrDefault();
+            var uye = db.TBLUYELER.Find(id);
+            //var uye = db.TBLUYELER.FirstOrDefault(x => x.MAIL == mail); // Üstteki iki satırı yazmadan bu satırdaki tek satırlık kod ile aynı işlemi yapabiliriz.
+            return PartialView("Partial2", uye);
         }
     }
 }
