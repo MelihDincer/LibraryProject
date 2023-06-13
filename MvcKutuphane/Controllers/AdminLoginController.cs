a57c69facbfef90d05785c397d2bbd80f7912776
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-13 13:52:47 +0300

    Admin controllerı eklendi. Login işlemi tamamlandı.

---
+++ MvcKutuphane/Controllers/AdminLoginController.cs
@@ -0,0 +1,36 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcKutuphane.Controllers
+{
+    [AllowAnonymous]
+    public class AdminLoginController : Controller
+    {
+        // GET: AdminLogin
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(TBLADMIN p)
+        {
+            var bilgiler = db.TBLADMIN.Where(x => x.Kullanici == p.Kullanici && x.Sifre == p.Sifre).FirstOrDefault();
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"]=bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Kategori");
+            }
+            else
+            {
+                return View();
+            }           
+        }
+    }
+}
