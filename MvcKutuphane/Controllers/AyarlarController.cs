c112d2bc1c53438f4dab889b87375d0059cd4fb6
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-14 16:32:09 +0300

    Ayarlar controllerı eklendi.

---
+++ MvcKutuphane/Controllers/AyarlarController.cs
@@ -0,0 +1,52 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class AyarlarController : Controller
+    {
+        // GET: Ayarlar
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var kullanicilar = db.TBLADMIN.ToList();
+            return View(kullanicilar);
+        }
+        [HttpGet]
+        public ActionResult YeniAdmin()
+        {
+            return View();
+        }
+        public ActionResult YeniAdmin(TBLADMIN p)
+        {
+            db.TBLADMIN.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult AdminSil(int id)
+        {
+            var adm = db.TBLADMIN.Find(id);
+            db.TBLADMIN.Remove(adm);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult AdminGetir(int id) 
+        {
+            var admin = db.TBLADMIN.Find(id);
+            return View("AdminGetir", admin); 
+        }
+        public ActionResult AdminGuncelle(TBLADMIN p)
+        {
+            var admin = db.TBLADMIN.Find(p.ID);
+            admin.Kullanici = p.Kullanici;
+            admin.Sifre = p.Sifre;
+            admin.Yetki = p.Yetki;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
