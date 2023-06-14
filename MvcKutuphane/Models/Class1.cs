8e90347f2a8c4fd08a813e6b31b8ef0dd98f7b8a
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-14 17:33:19 +0300

    Grafik için gerekli listenin oluşturulabilmesi amacıyla yeni bir class oluşturulup iki property tanımlaması yapıldı.

---
+++ MvcKutuphane/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcKutuphane.Models;
+
+namespace MvcKutuphane.Models
+{
+    public class Class1
+    {
+        public string yayinevi { get; set; }
+        public int sayi { get; set; }
+    }
+}
