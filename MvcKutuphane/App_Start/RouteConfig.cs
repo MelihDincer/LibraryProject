0f539fe0726095560fb98e8325919f2d42f805fc
Melih Dinçer <m_dincer41@hotmail.com>
2023-06-18 13:59:18 +0300

    Projenin route config ayarı yapılarak, başlangıç sayfası vitrin olarak düzenlendi.

--- MvcKutuphane/App_Start/RouteConfig.cs
+++ MvcKutuphane/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MvcKutuphane
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vitrin", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
